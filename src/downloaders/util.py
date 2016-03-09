b567f8c8446762c334ce770ed8b0d28f2b03622a
Tong Shu Li <tongli@scripps.edu>
2016-03-09 15:11:15 -0800

    Create function caching utility
    Create a decorator which tries to load the results of a function from a
    local file if it exists. If the file does not exist, then the decorator
    calls the function and caches the results to disk.
    
    This is often necessary when interfacing with web APIs and other slow
    processes.

---
+++ src/downloaders/util.py
@@ -0,0 +1,40 @@
+# Tong Shu Li
+
+import json
+import os
+import pytz
+
+from datetime import datetime
+
+def load_if_exist(file_loc):
+    """Load the function's output if it has already been cached to file.
+
+    Given a function, we first check to see if the results have already been
+    cached to disk, and return the cached version if it exists. If the cached
+    file doesn't exist, then create it after calling our function.
+
+    The JSON cache contains a timestamp, but does not return the timestamp.
+    """
+    file_loc = os.path.abspath(file_loc)
+    def decorator(function):
+        def wrapper(*args, **kwargs):
+            if os.path.isfile(file_loc):
+                with open(file_loc, "r") as fin:
+                    data = json.load(fin)
+
+                return data["data"]
+
+            data = function(*args, **kwargs)
+
+            cur_time = datetime.now(pytz.utc)
+            timestamp = cur_time.strftime("%Y-%m-%d %H:%M %Z")
+
+            result = {"timestamp": timestamp, "data": data}
+            with open(file_loc, "w") as fout:
+                json.dump(result, fout, indent = 4)
+
+            return data
+
+        return wrapper
+
+    return decorator
