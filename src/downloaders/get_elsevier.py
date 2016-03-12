44579ad06c12bb002ba2e5e9a3ec1e49bcd90ee6
Tong Shu Li <tongli@scripps.edu>
2016-03-12 13:34:00 -0800

    Rewrite Elsevier journal ISSN scraper asynchronously
    Update the Elsevier journal ISSN scraper to be asynchronous.

--- src/downloaders/get_elsevier.py
+++ src/downloaders/get_elsevier.py
@@ -68,43 +68,35 @@ def scrape_journal_titles():
     return fetch_and_map(get_titles, data, MAX_CONNECTIONS = 10, MAX_TIMEOUT = 10)
 
 
-def get_api_key():
-    floc = os.path.join("..", "..", "data", "elsevier", "apikey.txt")
-    with open(floc, "r") as fin:
-        api_key = fin.read().rstrip("\n")
-
-    return api_key
-
-
-@load_if_exist("../../data/elsevier/all_journal_issns.txt")
+#@load_if_exist("../../data/elsevier/all_journal_issns.txt")
 def scrape_all_journal_issns():
     """Use the Elsevier sitemap of all publications (books, journals) to
     determine the ISSNs of all journals.
     """
-    logger = logging.getLogger(__name__)
-    url = "http://api.elsevier.com/sitemap/page/sitemap/{}.html"
-
     # A small number of journals have two ISSNs, but most have unique ISSNs
+    def get_issns(html):
+        issn = defaultdict(list)
 
-    issn = defaultdict(list)
-    for letter in tqdm(string.ascii_lowercase):
-        error, html = fetch_page(url.format(letter))
+        soup = BeautifulSoup(html, "lxml")
+        for href in soup.find_all("a", href = re.compile(r'journals')):
+            name = href.text
+            link = href["href"]
 
-        if error is not None:
-            logger.warn("Sitemap for {} is broken".format(letter))
-        else:
-            soup = BeautifulSoup(html, "lxml")
-            for href in soup.find_all("a", href = re.compile(r'journals')):
-                name = href.text
-                link = href["href"]
+            # the ISSN with the middle dash removed
+            val = link[link.rfind("/") + 1 : link.rfind(".")]
+            assert len(val) == 8, "{} has a bad ISSN".format(name)
 
-                # the ISSN with the middle dash removed
-                val = link[link.rfind("/") + 1 : link.rfind(".")]
-                assert len(val) == 8, "{} has a bas ISSN".format(name)
+            issn[name].append("{}-{}".format(val[:4], val[4:]))
 
-                issn[name].append("{}-{}".format(val[:4], val[4:]))
+        return issn
 
-    return issn
+    url = "http://api.elsevier.com/sitemap/page/sitemap/{}.html"
+    data = {
+        letter: url.format(letter)
+        for letter in string.ascii_lowercase
+    }
+
+    return fetch_and_map(get_issns, data, MAX_CONNECTIONS = 8)
 
 
 def get_language(journal_issn):
@@ -227,11 +219,8 @@ def main():
 
 
     journal_titles = scrape_journal_titles()
-    cache("ttt.txt", journal_titles)
-    print("done")
-    return
-
     issns = scrape_all_journal_issns()
+    return
 
     lang = get_languages(journal_titles, issns)
 
