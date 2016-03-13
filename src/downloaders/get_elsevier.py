40f585638e6480ab6bc2dfacce6e965ea9422d89
Tong Shu Li <tongli@scripps.edu>
2016-03-12 21:23:23 -0800

    Rewrite English journal filtering lookup
    Rewrite looking up the languages of each journal to be an asynchronous
    process.

--- src/downloaders/get_elsevier.py
+++ src/downloaders/get_elsevier.py
@@ -34,6 +34,7 @@ import string
 
 from bs4 import BeautifulSoup
 from collections import defaultdict
+from itertools import chain
 from itertools import islice
 from tqdm import tqdm
 
@@ -44,7 +45,7 @@ from util import load_if_exist
 from web_util import fetch_and_map
 
 
-#@load_if_exist("../../data/elsevier/bio_journal_titles.txt")
+@load_if_exist("../../data/elsevier/bio_journal_titles.txt")
 def scrape_journal_titles():
     """Get the journal titles of only open access journals in the life or health
     sciences from ScienceDirect.
@@ -52,7 +53,7 @@ def scrape_journal_titles():
     Since the web interface has an annoying scrolling refresh, grab the journal
     titles grouped by starting letter.
     """
-    def get_titles(html):
+    def get_titles(key, html):
         soup = BeautifulSoup(html, "lxml")
         tags = soup.find_all("li", {"class": "browseimpBrowseRow"})
         return [tag.get("data-title") for tag in tags]
@@ -61,20 +62,21 @@ def scrape_journal_titles():
         "sub/5/18/17/220/22/21/466/23/487/{}/open-access")
 
     data = {
-        letter: url.format(letter)
+        letter: (url.format(letter), None)
         for letter in string.ascii_lowercase
     }
 
-    return fetch_and_map(get_titles, data, MAX_CONNECTIONS = 10, MAX_TIMEOUT = 10)
+    info = fetch_and_map(get_titles, data, MAX_CONNECTIONS = 10, MAX_TIMEOUT = 10)
+    return sorted(chain.from_iterable(info.values()))
 
 
-#@load_if_exist("../../data/elsevier/all_journal_issns.txt")
+@load_if_exist("../../data/elsevier/all_journal_issns.txt")
 def scrape_all_journal_issns():
     """Use the Elsevier sitemap of all publications (books, journals) to
     determine the ISSNs of all journals.
     """
     # A small number of journals have two ISSNs, but most have unique ISSNs
-    def get_issns(html):
+    def get_issns(key, html):
         issn = defaultdict(list)
 
         soup = BeautifulSoup(html, "lxml")
@@ -92,67 +94,76 @@ def scrape_all_journal_issns():
 
     url = "http://api.elsevier.com/sitemap/page/sitemap/{}.html"
     data = {
-        letter: url.format(letter)
+        letter: (url.format(letter), None)
         for letter in string.ascii_lowercase
     }
 
-    return fetch_and_map(get_issns, data, MAX_CONNECTIONS = 8)
+    info = fetch_and_map(get_issns, data, MAX_CONNECTIONS = 8)
+    res = {}
+    for val in info.values():
+        res.update(val)
+
+    return res
 
 
-def get_language(journal_issn):
-    """Lookup the publication language of each journal using the NLM catalog."""
-
-    logger = logging.getLogger(__name__)
-    def get_nlm_uid(issn):
-        url = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
-        params = {
-            "db": "nlmcatalog",
-            "term": "{}[ISSN]".format(issn),
-            "retmode": "json"
+@load_if_exist("../../data/elsevier/english_journals.txt")
+def get_open_eng_bio_journals():
+    """Return a dictionary of all English language biological science journals
+    that are open access, along with their ISSNs.
+    """
+    def get_journals():
+        titles = scrape_journal_titles()
+        issns = scrape_all_journal_issns()
+        return {
+            title: issns[title][0]
+            for title in titles
+                if title in issns and len(issns[title]) == 1
         }
 
-        error, json = fetch_page(url, params, rettype = "json")
-        if error is not None:
-            logger.warn("Error querying NLM Catalog with {}".format(issn))
-            return None
-
+    def get_nlm_uid(key, json):
         if int(json["esearchresult"]["count"]) == 1:
             return json["esearchresult"]["idlist"][0]
 
-        logger.info("No NLM id for {}".format(issn))
+        logger = logging.getLogger(__name__)
+        logger.warn("No NLM catalog UID for {} using ISSN for lookup".format(key))
         return None
 
-    def get_nlm_entry(nlm_uid):
-        url = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi"
-        params = {
+    def get_language(key, text):
+        for line in text.split("\n"):
+            if line.startswith("Language:"):
+                return list(set(line[len("Language: ") : ].split(", ")))
+
+        raise Exception("NLM Catalog missing language field for {}".format(key))
+
+
+    journals = get_journals()
+
+#-------------------------------------------------------------------------------
+
+    # look up the NLM catalog UIDs for each open journal with a unique ISSN
+    url = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
+    data = {
+        title: (url, {
             "db": "nlmcatalog",
-            "id": nlm_uid
-        }
-
-        error, text = fetch_page(url, params)
-        assert error is None, "Error for NLM UID {}".format(nlm_uid)
-
-        return text
-
-    nlm_uid = get_nlm_uid(journal_issn)
-    if nlm_uid is None:
-        return "unknown"
-
-    text = get_nlm_entry(nlm_uid)
-    lines = text.split("\n")
-    for line in lines:
-        if line.startswith("Language:"):
-            return line[len("Language: ") : ].split(", ")
-
-    raise Exception("NLM Catalog missing language field for {}".format(journal_issn))
-
-
-@load_if_exist("../../data/elsevier/open_journal_languages.txt")
-def get_languages(journal_titles, issns):
-    return {journal: get_language(issns[journal][0])
-        for journal in tqdm(journal_titles)
-            if journal in issns and len(issns[journal]) == 1
+            "term": "{}[ISSN]".format(issn),
+            "retmode": "json"
+        })
+        for title, issn in journals.items()
     }
+
+    nlm_uids = fetch_and_map(get_nlm_uid, data, rettype = "json", MAX_CONNECTIONS = 8)
+
+    # look up the language of each open journal with a NLM identifier
+    url = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi"
+    data = {
+        title: (url, {"db": "nlmcatalog", "id": uid})
+        for title, uid in nlm_uids.items() if uid is not None
+    }
+
+    langs = fetch_and_map(get_language, data, MAX_CONNECTIONS = 8)
+
+    # filter and return only those which are english
+    return sorted(title for title, lang in langs.items() if lang == ["English"])
 
 
 def scrape_piis_in_journal(url):
@@ -210,6 +221,7 @@ def scrape_all_piis(issns, languages):
     return piis
 
 
+
 def main():
     log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(
@@ -217,14 +229,8 @@ def main():
         level = logging.INFO, format = log_format
     )
 
+    eng_journals = get_open_eng_bio_journals()
 
-    journal_titles = scrape_journal_titles()
-    issns = scrape_all_journal_issns()
-    return
-
-    lang = get_languages(journal_titles, issns)
-
-    piis = scrape_all_piis(issns, lang)
     print("done")
 
 
