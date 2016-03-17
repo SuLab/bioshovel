48b924d833822068af14005d6e5e0beb431ead9b
Sandip Chatterjee <sandip.chatterjee@gmail.com>
2016-03-17 16:32:27 -0700

    Adds a script for running NER tools in parallel
    using multiple processes. This script uses Python's multiprocessing
    module to run Pubtator's tmChem Name-Entity Recognition tool in multiple
    processes.
    
    Input files for this script are of the format generated by XML parser scripts
    elsewhere in the `preprocess` module. Files are converted to the appropriate
    input format for tmChem using temporary directories specified by the Python
    `tempfile` module. Output files are found in the user-specified output
    directory.
    
    Error logging is supported via an additional thread run in the main process.
    
    Required and optional arguments and information are available by running
    `python3 chem_ner.py -h`

---
+++ src/preprocess/chem_ner.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python3
+
+# chem_ner.py
+
+# requires Perl to be available in $PATH
+
+# usage:
+# python3 chem_ner.py [path_to_paragraph_documents] [output_directory] --tmchem [path/to/tmChem.pl]
+
+# runs:
+# perl tmChem.pl -i inputdir -o outputdir -m Model/All.Model
+
+import argparse
+import logging
+import logging.handlers
+import os
+import subprocess
+import sys
+import tempfile
+import threading
+from glob import glob
+from itertools import repeat
+import multiprocessing as mp
+from tqdm import tqdm
+
+def save_file(file_name, file_info, directory):
+
+    ''' Saves file specified by file_info (a list of file lines, 
+        including newlines) to a new tempfile in directory
+    '''
+
+    with open(os.path.join(directory, file_name), 'w') as f:
+        for line in file_info:
+            f.write(line)
+
+
+def reformat_parsed_article(parsed_article_path):
+
+    ''' Read in file parsed_article_path with format:
+        title\tsome_title_text
+        [abs\t]some_abstract_text
+        p\tparagraph1_text
+        p\tparagraph2_text
+        ...
+
+        Output:
+        List of new file lines (including newlines) with format:
+        [escaped DOI]_a|article_title
+        [escaped_DOI]_a|article_abstract
+
+        [escaped DOI]_1|article_title
+        [escaped_DOI]_1|article_paragraph1
+
+        [escaped DOI]_2|article_title
+        [escaped_DOI]_2|article_paragraph2
+    '''
+
+    with open(parsed_article_path) as f:
+        title_line = f.readline().rstrip('\n')
+        if not title_line.startswith('title'):
+            print('Not a title line...')
+            return
+        body = [line.rstrip('\n') for line in f.readlines()]
+
+    article_title = title_line.split('\t')[1]
+    escaped_doi = os.path.basename(parsed_article_path)
+
+    new_file_lines = []
+    new_title_line = escaped_doi+'_{}|t|'+article_title
+
+    # if there is an abstract line, treat it separately
+    if body[0].startswith('abs\t'):
+        abstract = body[0].split('\t')[1]
+        new_file_lines.append(new_title_line.format('a'))
+        new_file_lines.append(escaped_doi+'_a|a|'+abstract)
+        new_file_lines.append('')
+        body = body[1:]
+
+    for line_num, line in enumerate(body):
+        new_file_lines.append(new_title_line.format(line_num+1))
+
+        # allows for tabs in the paragraph content...
+        paragraph_content = '\t'.join(line.split('\t')[1:])
+        new_file_lines.append(escaped_doi+
+                              '_{}|a|'.format(line_num+1)+
+                              paragraph_content)
+        new_file_lines.append('')
+
+    return (escaped_doi, [line+'\n' for line in new_file_lines])
+
+def process_and_run_chunk(filepaths_args_tuple):
+
+    ''' Generates reformatted files for each file path in 
+        list_of_file_paths, saves them to a single temp directory,
+        and calls tmChem using subprocess
+    '''
+
+    list_of_file_paths, args, q = filepaths_args_tuple
+
+    if not list_of_file_paths:
+        return
+
+    qh = logging.handlers.QueueHandler(q)
+    l = logging.getLogger()
+
+    reformatted_files = [reformat_parsed_article(file_path) 
+                         for file_path in list_of_file_paths]
+
+    with tempfile.TemporaryDirectory() as input_tempdir, tempfile.TemporaryDirectory() as output_tempdir:
+        for doi_filename, file_info in reformatted_files:
+            save_file(doi_filename, file_info, input_tempdir)
+
+        try:
+            subprocess.check_output(['perl', 
+                                     os.path.join(args.tmchem, 'tmChem.pl'), 
+                                     '-i', input_tempdir,
+                                     '-o', output_tempdir,
+                                     '-m', os.path.join(args.tmchem, 
+                                                        'Model', 
+                                                        'All.Model')
+                                     ],
+                                    cwd=args.tmchem)
+        except subprocess.CalledProcessError:
+            l.critical('tmChem error while processing chunk: {}'.format(list_of_file_paths))
+
+        all_tempfiles = glob(os.path.join(output_tempdir, '*'))
+
+        try:
+            subprocess.check_output(['cp', '-t', args.output_directory+'/'] + all_tempfiles)
+        except subprocess.CalledProcessError:
+            l.critical('Copy error, chunk: {}'.format(list_of_file_paths))
+
+def create_n_sublists(full_list, n=10):
+
+    ''' Returns a list of n sublists generated from full_list using stride of n
+    '''
+
+    return [full_list[i::n] for i in range(n)]
+
+def logging_thread(q):
+
+    ''' Handles logging to logfile while other processes are running
+
+        (exits upon receiving None in queue)
+    '''
+
+    while True:
+        record = q.get()
+        if record is None:
+            break
+        logger = logging.getLogger(record.name)
+        logger.handle(record)
+
+def main(args):
+
+    # check to make sure tmChem.pl exists
+    if not os.path.exists(os.path.join(args.tmchem,'tmChem.pl')):
+        print('tmChem.pl does not exist in path: {}'.format(args.tmchem))
+        sys.exit(1)
+
+    all_files = glob(os.path.join(args.paragraph_path, '*'))
+    filelist_with_sublists = create_n_sublists(all_files, mp.cpu_count()*10)
+
+    # check if save_directory exists and create if necessary
+    if not os.path.isdir(args.output_directory):
+        os.makedirs(args.output_directory)
+
+    log_filename = os.path.join(args.logdir, 'chem_ner.log')
+    logging_format = '%(asctime)s %(name)-15s %(levelname)-8s %(processName)-10s %(message)s'
+    logging.basicConfig(filename=log_filename,
+                        format=logging_format,
+                        level=logging.INFO,
+                        filemode='w')
+
+    print('Using {} cores to process {} files...'.format(mp.cpu_count(), 
+                                                         len(all_files)))
+
+    with mp.Pool() as pool:
+        mgr = mp.Manager()
+        q = mgr.Queue()
+        log_thread = threading.Thread(target=logging_thread, args=(q,))
+        log_thread.start()
+        imap_gen = pool.imap_unordered(process_and_run_chunk, 
+                                        zip(filelist_with_sublists, 
+                                            repeat(args),
+                                            repeat(q)))
+        for i in tqdm(imap_gen, total=len(filelist_with_sublists)):
+            pass
+
+    logging.info('Done processing {} files'.format(len(all_files)))
+    
+    # end logging_thread
+    q.put(None)
+    log_thread.join()
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Run tmChem on a directory of paragraph files')
+    parser.add_argument('paragraph_path', help='Directory of parsed paragraph files')
+    parser.add_argument('output_directory', help='Final output directory')
+    parser.add_argument('--tmchem', help='Directory where tmChem.pl is located', default=os.getcwd())
+    parser.add_argument('--logdir', help='Directory where logfile should be stored', default='../../logs')
+    args = parser.parse_args()
+    main(args)
