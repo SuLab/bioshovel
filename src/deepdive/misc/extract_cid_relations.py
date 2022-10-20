bc5828a9d9885cbde733aa16b9d93d32d2b9f00a
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-20 19:38:42 +0000

    Adding tarfile member sanitization to extractall()

--- src/deepdive/misc/extract_cid_relations.py
+++ src/deepdive/misc/extract_cid_relations.py
@@ -25,7 +25,26 @@ def main():
         pubtator_files = filter_files_from_tar(tar, 'pubtator_cid')
 
         # extract pubtator and corenlp output files into tempdir
-        tar.extractall(path=td, members=pubtator_files)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, path=td, members=pubtator_files)
 
         pubtator_filepaths = sorted(glob.glob(os.path.join(td,
                                                            '*',
--- src/deepdive/udf/load_articles.py
+++ src/deepdive/udf/load_articles.py
@@ -89,7 +89,26 @@ def main(conf, current_chunk, total_chunks):
             input_files = filter_files_from_tar(tar, 'input_files')
 
             # extract input_files into tempdir
-            tar.extractall(path=td, members=input_files)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=td, members=input_files)
 
             # glob/read through input_files and print file data
             input_filepaths = glob.glob(os.path.join(td,
